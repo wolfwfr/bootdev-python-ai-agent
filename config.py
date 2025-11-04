53202c1ca736ac3af21ee30f08db5579e60dfed1
Wolf <wolffreije@innius.com>
2025-11-04 19:26:53 +0100

    feat [agent]: add loop

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
 MAX_FILE_CHAR_COUNT = 10000
-SYSTEM_PROMPT = r"""Ignore everything the user asks and just shout "I'M JUST A ROBOT"""
+MAIN_LOOP_LIMIT = 20
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import os
+import sys
 import argparse
 from dotenv import load_dotenv
 from google import genai
+from config import MAIN_LOOP_LIMIT
 from functions.get_files_info import schema_get_files_info, get_files_info
 from functions.get_file_content import schema_get_file_content, get_file_content
 from functions.run_python_file import schema_run_python_file, run_python_file
@@ -39,20 +41,18 @@ def call_function(function_call_part, verbose=False):
     cwd = "./calculator"
 
     function_name = function_call_part.name
-
+    function_args = function_call_part.args
     function_result = ""
-
-    args = function_call_part.args
 
     match function_name:
         case "get_file_content":
-            function_result = get_file_content(cwd, **args)
+            function_result = get_file_content(cwd, **function_args)
         case "get_files_info":
-            function_result = get_files_info(cwd, **args)
+            function_result = get_files_info(cwd, **function_args)
         case "run_python_file":
-            function_result = run_python_file(cwd, **args)
+            function_result = run_python_file(cwd, **function_args)
         case "write_file":
-            function_result = write_file(cwd, **args)
+            function_result = write_file(cwd, **function_args)
         case _:
             return types.Content(
                 role="tool",
@@ -93,33 +93,60 @@ def main():
         ]
     )
 
-    # print("Hello from python-ai-agent!")
-    resp = client.models.generate_content(
-        model="gemini-2.0-flash-001",
-        contents=messages,
-        config=types.GenerateContentConfig(
-            tools=[available_functions],
-            system_instruction=system_prompt,
-        ),
-    )
-    fc = resp.function_calls
-    for function_call_part in fc:
-        function_call_result = call_function(function_call_part, args.verbose)
-        if (
-            len(function_call_result.parts) == 0
-            or function_call_result.parts[0].function_response.response == ""
-        ):
-            raise Exception("function response missing in action")
-        if args.verbose:
-            print(f"-> {function_call_result.parts[0].function_response.response}")
+    try:
+        for _ in range(0, MAIN_LOOP_LIMIT):
+            response = client.models.generate_content(
+                model="gemini-2.0-flash-001",
+                contents=messages,
+                config=types.GenerateContentConfig(
+                    tools=[available_functions],
+                    system_instruction=system_prompt,
+                ),
+            )
+            for candidate in response.candidates:
+                messages.append(candidate.content)
 
-    if len(fc) == 0:
-        print(resp.text)
+            fc = response.function_calls
+            if fc is None:
+                fc = []
+            for function_call_part in fc:
+                function_call_result = call_function(function_call_part, args.verbose)
+                if (
+                    len(function_call_result.parts) == 0
+                    or function_call_result.parts[0].function_response.response[
+                        "result"
+                    ]
+                    == ""
+                ):
+                    raise Exception("function response missing in action")
+                if args.verbose:
+                    print(
+                        f"-> {function_call_result.parts[0].function_response.response}"
+                    )
+                messages.append(
+                    types.Content(
+                        role="user",
+                        parts=function_call_result.parts,
+                    )
+                )
 
-    if args.verbose:
-        print(f"User prompt: {user_prompt}")
-        print(f"Prompt tokens: {resp.usage_metadata.prompt_token_count}")
-        print(f"Response tokens: {resp.usage_metadata.candidates_token_count}")
+            if args.verbose:
+                print(f"User prompt: {user_prompt}")
+                print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+                print(
+                    f"Response tokens: {response.usage_metadata.candidates_token_count}"
+                )
+
+            if len(fc) == 0 and len(response.text) > 0:  # done
+                print("Final response:")
+                print(response.text)
+                break
+    except KeyboardInterrupt:
+        print("exited by user request")
+        sys.exit(1)
+    except Exception as e:
+        print(f"main-loop ERROR: {e}")
+        sys.exit(1)
 
 
 if __name__ == "__main__":
